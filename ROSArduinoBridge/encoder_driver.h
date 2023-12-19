42511cf3ada834aefb9a9b6e1c10c1415a9ba452
Lars_Ubuntu <lars@hielscher.dev>
2023-12-19 09:37:23 +0100

    Changed Pin configuration

--- ROSArduinoBridge/encoder_driver.h
+++ ROSArduinoBridge/encoder_driver.h
@@ -6,12 +6,12 @@
 #ifdef ARDUINO_ENC_COUNTER
   //below can be changed, but should be PORTD pins; 
   //otherwise additional changes in the code are required
-  #define LEFT_ENC_PIN_A PD2  //pin 2
-  #define LEFT_ENC_PIN_B PD4  //pin 3
+  #define LEFT_ENC_PIN_A 2  //pin 2 - motor 1 a 
+  #define LEFT_ENC_PIN_B 7  //pin 7 - motor 1 b 
   
   //below can be changed, but should be PORTC pins
-  #define RIGHT_ENC_PIN_A PC4  //pin A4
-  #define RIGHT_ENC_PIN_B PC5  //pin A5
+  #define RIGHT_ENC_PIN_A 17  //pin A2 - motor 2 a
+  #define RIGHT_ENC_PIN_B 16  //pin A3 - motor 2 b
 
   //below can be changed, but should be PORTB pins
   #define TOOL_ENC_PIN_A PB11  //pin 11
@@ -21,4 +21,3 @@
 long readEncoder(int i);
 void resetEncoder(int i);
 void resetEncoders();
-
